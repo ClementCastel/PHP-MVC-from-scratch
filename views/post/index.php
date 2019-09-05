998e8a29d23db64f581c368f9af15916d4754d13
Clément Castel <clement.castel75@gmail.com>
2019-09-05 20:19:13 +0200

    Post pagination

--- views/post/index.php
+++ views/post/index.php
@@ -7,8 +7,28 @@ $title = "Mon blog";
 $pdo = new PDO('mysql:dbname=blogmvc;host=127.0.0.1', 'root','root', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
+$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
 
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+$perPages = 12;
+$pages = ceil($count / $perPages);
+
+$page = $_GET['page'] ?? 1;
+if (!filter_var($page, FILTER_VALIDATE_INT)) throw new Exception('Numéro de page invalide');
+if ($page === "1") {
+    header('Location: ' . $router->url('home'));
+    http_response_code(301);
+    exit();
+}
+$currentPage = (int)$page;
+
+if ($currentPage < 1 || $currentPage > $pages) {
+    throw new Exception ("Numéro de page invalide");
+}
+
+
+
+
+$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT ' . $perPages . ' OFFSET ' . ($currentPage-1) * $perPages);
 
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 ?>
@@ -21,4 +41,17 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
             <?php require 'card.php' ?>
         </div>
     <?php endforeach ?>
+</div>
+
+<div class="d-flex justify-content-between my-4">
+    <?php if ($currentPage > 1): ?>
+        <?php
+            $link = $router->url('home');
+            if ($currentPage > 2) $link .= '?page=' . ($currentPage - 1);
+        ?>
+        <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page précédente</a>
+    <?php endif ?>
+    <?php if ($currentPage < $pages): ?>
+        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
+    <?php endif ?>
 </div>
